ef171dca7e6ab213ece1ffffd31abb46c475d82e
Sandip Sharma <82011491+MrSandipSharma@users.noreply.github.com>
2023-05-22 20:56:21 +0530

    Add files via upload

---
+++ Newbie/QR code component/script.js
@@ -0,0 +1,5 @@
+const tl = new TimelineMax();
+
+tl.from(".card_qr-code", 0.3, { opacity: "0", y: "30%" });
+tl.from(".card_heading", 0.3, { opacity: "0", y: "10%" });
+tl.from(".card_description", 0.3, { opacity: "0", y: "10%" });
